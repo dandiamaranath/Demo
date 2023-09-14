6ca9d9df7f69fa7558adfb3c9d70508f17b92800
dandiamaranath <amaranathdandi@gmail.com>
2023-09-14 21:53:54 +0530

    Inheritence concept

---
+++ Inheritence/src/Inheritenceconcept/Multiple.java
@@ -0,0 +1,24 @@
+package Inheritenceconcept;
+
+public class Multiple {
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Book();
+		    Pen();
+		    bag();
+		    
+				 
+			}
+			public static void Book() {                   
+				System.out.println("White book");
+			}
+			public static void Pen() {                    
+				System.out.println("bule,red");
+			}
+			public static void bag() {
+				System.out.println("colou,block");
+		
+	}
+
+}
---
+++ Inheritence/src/Inheritenceconcept/Single.java
@@ -0,0 +1,20 @@
+package Inheritenceconcept;
+
+public class Single {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Book();
+	    Pen();
+			 
+		}
+		public static void Book() {                    //Parent class
+			System.out.println("Bookshop");
+		}
+		public static void Pen() {                     //child class
+			System.out.println("bule");
+		}
+	
+	}
+
+
